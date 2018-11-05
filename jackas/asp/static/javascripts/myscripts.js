661126ef249c1a56b368ed15351f465b31622d20
unknown <chengc5@illinois.edu>
2018-11-05 22:44:58 +0800

    modified total weight calculation

--- jackas/asp/static/javascripts/myscripts.js
+++ jackas/asp/static/javascripts/myscripts.js
@@ -1,6 +1,7 @@
 
 var totalWeight = 0.0;
 var currentOrder = {};
+var weights = {};
 var maxWeight = 25.0;
 var currentCat = "";
 
@@ -33,33 +34,18 @@ function constructOrder(userid){
               .submit();
 }
 
-
-function constructOrder1(userid){
-    console.log("clicked");
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-
-        }
-    };
-    num_order = Object.keys(currentOrder).length;
-    priority = document.getElementById("priority_list").value;
-
-    request_str = "http://127.0.0.1:8000/asp/clinic_manager/construct_order?num="+num_order+"&weight="+totalWeight+"&priority="+priority+"&user="+userid;
-
-    var i = 0;
-    for (key in currentOrder){
-        request_str += "&item"+i+"="+key + "&item"+i+"num"+"="+currentOrder[key];
-    }
-
-    xhttp.open("GET", request_str, true);
-    xhttp.send();
-}
-
 function initiateVar(){
     totalWeight = 0.0;
     currentOrder = {};
     maxWeight = 25.0;
+}
+
+function calculateTotalWeight(){
+    w = 0.0;
+    for (key in currentOrder){
+        w += currentOrder[key] * weights[key];
+    }
+    return parseFloat(w.toFixed(2));
 }
 
 function changeColor(category){
@@ -76,33 +62,41 @@ function changeColor(category){
 }
 
 function updateVar(itemid, quantity){
+        currentOrder[itemid] = quantity; 
+        totalWeight = calculateTotalWeight();
+        console.log("totalWeight:"+totalWeight);
+        document.getElementById("totalWeight").value = totalWeight;
         if (document.getElementById("totalWeight").value > maxWeight){
             document.getElementById("warning").innerHTML = "Order too heavy!";
         }else{ 
             document.getElementById("warning").innerHTML = "";
         }
-        currentOrder[itemid] = quantity;    
+        console.log("updated");   
 }
 
 function updateOrder(itemid, itemweight, quantity){
+    console.log("updateOrder");
     exist = false;
     for (key in currentOrder){
         if (key==itemid){
             exist = true; break;
         }
     }
-    quant = parseInt(quantity);
-    itemwei = parseInt(itemweight);
+    quant = quantity;
+    itemwei = itemweight;
+    console.log("quant"+quant);
+    console.log("itemwei"+itemwei);
     if (exist){
-        totalWeight += itemwei * (quant - currentOrder[itemid])
-        document.getElementById("totalWeight").value = totalWeight;  
+        console.log("exists");
+
         updateVar(itemid,quant); 
         if (quant==0){
             delete currentOrder[itemid];
         }    
     } else {
-        totalWeight += itemwei * quant;
-        document.getElementById("totalWeight").value = totalWeight;
+        console.log("doesnt exist");
+        newweight =  itemwei * quant;
+        weights[itemid] = itemwei;
         updateVar(itemid,quant);
     }
 }
