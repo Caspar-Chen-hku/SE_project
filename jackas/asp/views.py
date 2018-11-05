02ae05f3b82d6656c628cf409cf8b63486e84370
James-QiuHaoran <jamesqiu@hku.hk>
2018-11-05 21:35:54 +0800

    add status change

--- jackas/asp/views.py
+++ jackas/asp/views.py
@@ -154,7 +154,7 @@ class DispatcherViewPackage(ListView):
 		
 class DispatcherViewItinerary(ListView):
 	"""docstring for DispatcherViewQueue"""
-	def get(self, request):
+	def post(self, request):
 		clinic_distance_list= Distance.objects.all()
 		clinic_list=Clinic.objects.all()
 		distance={}
@@ -167,7 +167,7 @@ class DispatcherViewItinerary(ListView):
 			clinic[elem.pk]=(elem.latitude,elem.longitude,elem.altitude,elem.distance_to_hospital)
 			clinic_id +=1
 
-		package = request.POST.get('package',None)# a list of order objects
+		package = request.POST.get('package', None)# a list of order objects
 		route_list=[]
 		for order in package:
 			route_list.append(order.destination_id)
